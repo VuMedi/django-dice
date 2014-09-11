a99076a4de6bd37d205bd749b521c99262ef32d1
Robert Winder <robert.winder@gmail.com>
2014-09-11 11:16:04 -0700

    Enable manager to pick from queryset.

--- dice/managers.py
+++ dice/managers.py
@@ -2,8 +2,17 @@ from django.db import models
 import random
 
 class ChoiceManager(models.Manager):
+    use_for_related_fields = True
+
+    def pick_from_set(self):
+        candidates = self.get_queryset()
+        return self.return_pick(candidates)
+
     def pick(self, key):
         candidates = self.filter(key=key)
+        return self.return_pick(candidates)
+
+    def return_pick(self, candidates):
         if len(candidates) == 0:
             raise Exception("There are no records with this key.")
         weight_sum = sum([candidate.weight for candidate in candidates])
